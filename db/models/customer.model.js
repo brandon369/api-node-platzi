3265aa7cae6a7c4bcfc6fe678614164f2db76513
Brandon A <byarrieta@protonmail.com>
2022-07-06 20:08:56 -0500

    relation many to many

--- db/models/customer.model.js
+++ db/models/customer.model.js
@@ -1,6 +1,9 @@
 const { Model, DataTypes, Sequelize } = require("sequelize");
+
 const { USER_TABLE } = require("./user.model");
+
 const CUSTOMER_TABLE = "customers";
+
 const CustomerSchema = {
   id: {
     allowNull: false,
@@ -8,9 +11,19 @@ const CustomerSchema = {
     primaryKey: true,
     type: DataTypes.INTEGER,
   },
-  name: { allowNull: false, type: DataTypes.STRING },
-  lastName: { allowNull: false, type: DataTypes.STRING, field: "last_name" },
-  phone: { allowNull: true, type: DataTypes.STRING },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  lastName: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    field: "last_name",
+  },
+  phone: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
   createdAt: {
     allowNull: false,
     type: DataTypes.DATE,
@@ -22,17 +35,23 @@ const CustomerSchema = {
     allowNull: false,
     type: DataTypes.INTEGER,
     unique: true,
-    references: { model: USER_TABLE, key: "id" },
+    references: {
+      model: USER_TABLE,
+      key: "id",
+    },
     onUpdate: "CASCADE",
     onDelete: "SET NULL",
   },
 };
-class Customer extends Model {
 
+class Customer extends Model {
   static associate(models) {
     this.belongsTo(models.User, { as: "user" });
+    this.hasMany(models.Order, {
+      as: "orders",
+      foreignKey: "customerId",
+    });
   }
-
 
   static config(sequelize) {
     return {
@@ -43,4 +62,5 @@ class Customer extends Model {
     };
   }
 }
+
 module.exports = { Customer, CustomerSchema, CUSTOMER_TABLE };
